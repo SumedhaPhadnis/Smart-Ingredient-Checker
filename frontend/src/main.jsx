a69dd4459e3467f2cbacf419a050b554526375cb
SumedhaPhadnis <82096691+SumedhaPhadnis@users.noreply.github.com>
2026-06-23 12:10:20 +0530

    Add theme management from localStorage
    Retrieve and apply saved theme from localStorage.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,9 @@ import App from './App'
 import './index.css'
 
 const clientId = "637812395299-742oti3v5imh087jrpd147nbhfmi0tt1.apps.googleusercontent.com";
-
+const savedTheme = localStorage.getItem("theme") || "light";
+document.documentElement.classList.remove("light", "dark", "amoled");
+document.documentElement.classList.add(savedTheme);
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <GoogleOAuthProvider clientId={clientId}>
